8d6ed37cd3741764cc6f9d7a05f0e2ecdeeb5884
Niels Hamelink <nielshamelink@gmail.com>
2015-04-23 18:15:14 +0200

    Update 1
    .classpath added
    author in plugin.yml added
    Email validate check in RegisterCommand.java added

--- src/com/nielsha/plugins/commands/RegisterCommand.java
+++ src/com/nielsha/plugins/commands/RegisterCommand.java
@@ -4,6 +4,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -16,18 +18,18 @@ import com.nielsha.plugins.managers.MessageManager;
 import com.nielsha.plugins.managers.MysqlManager;
 
 public class RegisterCommand implements CommandExecutor {
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(!(sender instanceof Player)){
 			return true;
 		}
 		Player p = (Player) sender;
-		
+
 		if(args.length != 3){
 			p.sendMessage(MessageManager.getMessage("WRONG_ARGUMENTS"));
 			return true;
 		}
-		
+
 		try {
 			Statement s = MysqlManager.getConnection().createStatement();
 			ResultSet set = s.executeQuery("SELECT * FROM users WHERE uuid='" + p.getUniqueId().toString() + "'");
@@ -35,23 +37,33 @@ public class RegisterCommand implements CommandExecutor {
 				p.sendMessage(MessageManager.getMessage("ALREADY_REGISTERED"));
 				return true;
 			}
-			
+
 			if(!args[1].equalsIgnoreCase(args[2])){
 				p.sendMessage(MessageManager.getMessage("PASSWORDS_NOT_EQUAL"));
 				return true;
 			}
 			
+			if(!RegisterCommand.validate(args[0]))
+				return true;
+
 			PreparedStatement pre = MysqlManager.getConnection().prepareStatement("INSERT INTO `users` (Username, Password, UUID, Email) VALUES (?, ?, ?, ?)");
 			pre.setString(1, p.getName());
 			pre.setString(2, CoreManager.hash(args[0], new Core().getConfig().getString("Hash")));
 			pre.setString(3, p.getUniqueId().toString());
 			pre.setString(4, args[0]);
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return false;
+	}
+
+	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+	public static boolean validate(String emailStr) {
+		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
+		return matcher.find();
 	}
 
 }
